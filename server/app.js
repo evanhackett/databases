30034365390a2548e2dbc61bcf3e9f3c99a23aed
evanhackett <evanwhackett@gmail.com>
2015-04-19 23:09:09 -0700

    stores messages into databse. Still need to store usernames into database.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var db = require('./db');
+// var mysql = require('mysql');
 
 // Middleware
 var morgan = require('morgan');
@@ -9,6 +10,50 @@ var parser = require('body-parser');
 var router = require('./routes.js');
 
 var app = express();
+
+//connection.query('USE chat');
+
+
+//dealing with CORS
+
+// app.all('*', function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// app.use(express.static(__dirname + "/../client"));
+
+// app.get('/', function (req, res) {
+//   console.log("ENTERS THE GET REQUEST");
+//   res.writeHead(200);
+//   res.end('Hello World!');
+//   // instead of res.writeHead and res.end, we can just call res.send
+//   //res.send("Hello woorld");
+// });
+
+// app.post('/', function (req, res) {
+//   console.log("ENTERS THE POST REQUEST");
+//     req.on('data', function(data) {
+//       console.log("ENTERS THE POST REQUEST- DATA");
+//       console.log(JSON.parse(data));
+//       connection.query("INSERT into messages (username) values ('Mike')", req.body, 
+//           function (err, result) {
+//               if (err) throw err;
+//               console.log("Enters database function");
+//               // res.send('User added to database with ID');
+//           }
+//       );
+
+//     });
+
+      
+//   res.send('POST request to the homepage');
+// });
+
+
+
 module.exports.app = app;
 
 // Set what we are listening on.
@@ -20,9 +65,12 @@ app.use(parser.json());
 
 // Set up our routes
 app.use("/classes", router);
+// app.use("/", router);
+
 
 // Serve the client files
 app.use(express.static(__dirname + "/../client"));
+
 
 // If we are being run directly, run the server.
 if (!module.parent) {
--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,47 @@
 var db = require('../db');
 
 
-
-
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      var queryStr = "select messages.id, messages.text, messages.roomname, users.username \
+                      from messages left outer join users on (messages.userid = users.id) \
+                      order by messages.id desc";
+      db.query(queryStr, function (err, results) {
+        callback(results);
+      });
+    }, // a function which produces all the messages
+    post: function (params, callback) {
+      var queryStr = "insert into messages(text, userid, roomname) \
+                      value (?, (select id from users where username = ? limit 1), ?)";
+      db.query(queryStr, params, function (err, results) {
+        console.log(queryStr);
+        console.log(JSON.stringify(params));
+        callback(results);
+      });
+   } 
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      var queryStr = "select * from users";
+      db.query(queryStr, function(err, results){
+        callback(err, results);
+      });
+    },
+    post: function (params, callback) {
+      var queryStr = "insert into users(username) values (?)";
+      db.query(queryStr, params, function(err, results){
+        callback(err, results);
+      });
+    }
   }
 };
 
+
+
+// var queryStr = "insert into messages(text, userid, roomname) \
+//                       value (?, (select id from users where username = ? limit 1), ?)";
+
+// var queryStr = "insert into messages(text, userid, roomname) value (" + username + ", " + text +")";
